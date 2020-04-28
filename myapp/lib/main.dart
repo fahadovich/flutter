a77634cf839ae3dc2fc3feff7d0432bcaadf82b8
unknown <fahadovich@gmail.com>
2020-04-28 12:14:18 +0300

    test msg

---
+++ myapp/lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp()); // START WIDGET TREE
+
+class MyApp extends StatelessWidget {
+
+void answer(){
+  print('hiii');
+}
+
+  @override
+  build(BuildContext context) {
+    var q = ['q1', 'q2', 'q3'];
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text('My first app4')),
+        body: Column(
+          children: <Widget>[
+            Text(q.elementAt(0)),
+            RaisedButton(
+              child: Text('Ans1'),
+              onPressed: answer,
+            ),
+            RaisedButton(
+              child: Text('Ans1'),
+              onPressed: () => print('Yessss!!'),
+            ),
+            RaisedButton(
+              child: Text('Ans1'),
+              onPressed: null,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
